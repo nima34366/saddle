3b7883d4bbcc626cb04a6dec8f43cdda86cc6af5
nima34366 <wickramasinghenlssck@gmail.com>
2023-05-26 13:45:03 +1000

    deleted unnecessary

--- nima.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import wandb
-wandb.init(project="saddle",entity="nimawickramasinghe")
-
-plt.semilogy([1,2,3,4],[1,4,9,16], 'ro')
-plt.tight_layout()
-plt.legend([f'A simple {2*3} line <br> secondline'], handlelength=0, loc='center left')
-# plt.show()
-wandb.log({"chart": plt})
